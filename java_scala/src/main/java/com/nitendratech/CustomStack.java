227dfa0f4dd6356ebf98eacc3f6f807ef122055b
Nitendra Gautam <nitendragautam@Nitendras-MacBook-Pro.local>
2020-04-06 10:24:41 -0500

    Adding new Projects to git

---
+++ java_scala/src/main/java/com/nitendratech/CustomStack.java
@@ -0,0 +1,23 @@
+package com.nitendratech;
+
+
+/**
+* Stack can have custom API like 
+* push(argument)
+* and pop() ==> returns an item
+*
+* Stack has Last In First Out Architecture(LIFO) and 
+* FIFO (First In Last Out Architecture)
+*  
+*/
+
+public class CustomStack{
+
+
+public static void main(String args[]){
+
+
+}
+
+}
+
