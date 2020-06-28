b757fb5bb2eae3e6b1155d3bc15e549a289e346c
nitendragautam <nitendra.gautam12@gmail.com>
2020-06-28 13:17:06 -0500

    Adding code on before and AFterAll

--- javatesting/src/test/java/com/nitendratech/javatesting/junit5/MathUtilsTest.java
+++ javatesting/src/test/java/com/nitendratech/javatesting/junit5/MathUtilsTest.java
@@ -1,6 +1,9 @@
 package com.nitendratech.javatesting.junit5;
 
 import com.nitendratech.javatesting.MathUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,12 +15,29 @@ public class MathUtilsTest {
 
     MathUtils mathUtils;
 
+    @BeforeAll
+    static void beforeAllInit(){
+        System.out.println("This needs to run before even the MathUtils object is created");
+
+    }
+
+    /*
+    This will run before each of the test Instance runs
+     */
+    @BeforeEach
+    void init(){
+        mathUtils = new MathUtils();
+    }
 
 
+    @AfterEach
+    void cleanup(){
+        System.out.println("Cleaning Up");
+    }
 
     @Test
     void test(){
-        MathUtils mathUtils = new MathUtils();
+
         int expected =4;
         int actual = mathUtils.add(2,2);
 
@@ -26,13 +46,18 @@ public class MathUtilsTest {
         assertEquals(6,mathUtils.add(3,3));
     }
 
+    /*
+    Throwing Exception in the Test Cases
+     */
+    @Test
+    void testDivide(){
+        assertThrows(ArithmeticException.class, () -> mathUtils.divide(1,0),"Divide by zero should throw Exception");
+
+    }
 
 
     @Test
     void testComputeSircleRadius(){
-        MathUtils mathUtils = new MathUtils();
-
-
         assertEquals(314.1592653589793, mathUtils.computeCircleArea(10),"Should return Circle Area");
 
     }
